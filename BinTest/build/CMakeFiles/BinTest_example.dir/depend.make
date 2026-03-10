91163ffd84538625d9b431b2d648c2484b2ce2b8
sgly123 <2388594519@qq.com>
2026-03-10 19:52:01 +0800

    删除无用文件

--- BinTest/build/CMakeFiles/BinTest_example.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BinTest_example.
-# This may be replaced when dependencies are built.
--- BinTest/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sgly/桌面/NetBox/BinTest
-# Build directory: /home/sgly/桌面/NetBox/BinTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
